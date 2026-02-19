82cb8f6abf508215ad1d1c7ec51c7c45add0464f
Fedotova Maria <159981092+mariazlygosteva@users.noreply.github.com>
2026-02-19 10:10:52 +0700

    task_4.py

--- task_4.py
+++ task_4.py
@@ -1,10 +1,10 @@
 def task4() -> None:
     '''
     Reads two lines of space-separated integers, converts each to a set,
-    then reads a single integer x. Prints 'YES' if x belongs to both sets,
-    otherwise prints 'NO'.
+    then reads a single integer x. Prints 'YES' if x belongs to both sets
+    (i.e., x is in the intersection of the two sets), otherwise prints 'NO'.
     '''
-    set1 = set(map(int, input().split()))
-    set2 = set(map(int, input().split()))
-    x = int(input())
-    print('YES' if x in set1 and x in set2 else 'NO')
+    first_set = set(map(int, input().split()))
+    second_set = set(map(int, input().split()))
+    number = int(input())
+    print('YES' if number in (first_set & second_set) else 'NO')
